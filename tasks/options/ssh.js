53e222253ccea6941d54b01d5c263cc821717780
joeymilanes <jmilanes@wasabirabbit.com>
2015-01-21 17:12:20 -0500

    Updates

---
+++ tasks/options/ssh.js
@@ -0,0 +1,15 @@
+module.exports = {
+sftp: {
+	deploy:{
+    files: {
+      "./": "index.html"
+    },
+    options: {
+      host: '<%= grunt02.wasabirabb.it %>',
+      username: '<%=  http://grunt02.wasabirabb.it/jmilanes@wasabirabbit.com %>',
+      password: '<%= celtics$5 %>',
+      showProgress: true
+    }
+  } 
+}
+}
