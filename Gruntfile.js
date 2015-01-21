e463196c7689452952f85f9ba66fe93406e3a0c9
joeymilanes <jmilanes@wasabirabbit.com>
2015-01-21 11:05:35 -0500

    Taks/opt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,9 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'js/WRR_main.js',
+        dest: 'js/WRR_main.min.js'
       }
     }
   });
